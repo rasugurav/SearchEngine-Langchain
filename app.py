123879d7edaf9076d26346a2ffb41d9363f74127
Rasika R Gurav <63301668+rasugurav@users.noreply.github.com>
2026-06-06 08:09:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ if prompt:=st.chat_input(placeholder="What is machine learning?"):
     tools=[search,wiki,arxiv]
 
 
-    search_agent=initialize_agent(tools,llm,agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,handling_parsing_error=True)
+    search_agent=initialize_agent(tools,llm,agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,handle_parsing_errors=True)
 
 
     with st.chat_message("assistant"):
