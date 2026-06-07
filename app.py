6fcc380c55d465eb10fa3660a11065859ef558b3
Rasika R Gurav <63301668+rasugurav@users.noreply.github.com>
2026-06-07 08:07:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ if prompt:=st.chat_input(placeholder="What is machine learning?"):
     search_agent=initialize_agent(tools,
                                   llm,
                                   agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-                                  handle_parsing_errors=True,
+                                  handle_parsing_errors=False,
                                  verbose=True
                                  )
 
